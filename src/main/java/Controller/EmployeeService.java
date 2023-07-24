8c51f1b79338fc7e53f01e178b9bff56661c5457
Сергей <bondarevser.bondarev@yandex.ru>
2023-07-24 14:03:38 +0300

    1

---
+++ src/main/java/Controller/EmployeeService.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import com.example.model.Employee;
+
+import java.util.Collection;
+
+public class EmployeeService {
+    public Employee add(String firstName, String lastName) {
+        return null;
+    }
+
+    public Collection<Employee> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/l_s/LSApplication.java
@@ -0,0 +1,13 @@
+package com.example.l_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LSApplication.class, args);
+    }
+
+}
