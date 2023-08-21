1bb4a7876ad504e298c6023f5418b283a9df2f95
Сергей <bondarevser.bondarev@yandex.ru>
2023-08-21 18:42:18 +0300

    commit 5

---
+++ src/test/java/com/example/l_s/LSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
