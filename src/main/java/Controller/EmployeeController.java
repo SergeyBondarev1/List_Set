3a06f1092565f313cf67cfdf1252a4f8b4ea65b0
Сергей <bondarevser.bondarev@yandex.ru>
2023-08-21 18:42:02 +0300

    commit 4

--- src/main/java/Controller/EmployeeController.java
+++ src/main/java/Controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package Controller;
 
 import com.example.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,12 +20,15 @@ public class EmployeeController {
     public EmployeeController(EmployeeService service) {
         this.service = service;
     }
-
     @GetMapping("/add")
     public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        return service.add(firstName, lastName);
+        if (!StringUtils.isAlpha(firstName) || !StringUtils.isAlpha(lastName)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        service.addEmployee(firstName,lastName);
+        return ResponseEntity.ok().build();
     }
-
+    servese.
     @GetMapping("/remove")
     public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
         return service.add(firstName, lastName);
